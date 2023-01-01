bd7d101ea2591f362227d6158981326b884ba2d8
Mosesoluwa1 <mosesoluwa2005@gmail.com>
2023-01-01 14:46:03 -0800

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *  _strncat - concatenates two strings
+ *
+ *  @src: The source of strings
+ *  @dest: The destination of the string
+ *  @n: The length of int
+ *
+ *  Return: pointer to the resulting string dest
+ */
